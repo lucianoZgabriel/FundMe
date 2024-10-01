ce546e08f02f48217e48a9a06be5bd98c2104a61
lucianoZgabriel <lucianozanin@gmail.com>
2024-09-30 21:58:08 -0400

    add more tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,26 +2,31 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import { FundMe } from "../src/FundMe.sol";
+import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("USER");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
-      DeployFundMe deployFundMe = new DeployFundMe();
-      fundMe = deployFundMe.run();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
-    function testMinimumDollarIsFive() public {
-      assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
     }
 
-    function testOwnerIsMsgSender() public {
-      assertEq(fundMe.i_owner(), msg.sender);
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
-    function testPriceFeedVersionIsAccurate() public {
+    function testPriceFeedVersionIsAccurate() public view {
         try fundMe.getVersion() returns (uint256 version) {
             console.log("Price Feed Version: ", version);
             // Verifique se a versão é maior que 0 em vez de igual a 4
@@ -35,9 +40,70 @@ contract FundMeTest is Test {
         }
     }
 
-    /*function testPriceFeedVersionIsAccurate() public {
-      uint256 version = fundMe.getVersion();
-      console.log("Price Feed Version: ", version);
-      assertEq(version, 4);
-    }*/
+    function testFundFailsWithInsufficientEth() public {
+        vm.expectRevert("You need to spend more ETH!");
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.addressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFundersToArray() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFunderBalance = address(fundMe).balance;
+
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFunderBalance = address(fundMe).balance;
+
+        assertEq(endingFunderBalance, 0);
+        assertEq(
+            endingOwnerBalance,
+            startingOwnerBalance + startingFunderBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunder() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFundersIndex = 2;
+        for (uint160 i = startingFundersIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFunderBalance = address(fundMe).balance;
+
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFunderBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
 }
